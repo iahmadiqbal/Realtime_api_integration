1447d7a8babc64a59b84b3bae0eabcc83ce83706
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-11-23 02:25:35 -0800

    layout.js file updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,3 @@
-// import "./globals.css";
-
-// export const metadata = {
-//   title: "CRUD OPERATION",
-//   description: "Crud operation using tanstack Query",
-// };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
